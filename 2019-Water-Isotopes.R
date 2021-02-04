9b55a22aa197250c8ca25aeb1f3dd4a6bb9d2e45
Ben Bolker <bbolker@gmail.com>
2021-02-04 10:19:48 -0500

    instructor HW comments

--- 2019-Water-Isotopes.R
+++ 2019-Water-Isotopes.R
@@ -3,9 +3,14 @@
 
 library(tidyverse)
 
-df<-read.csv("./2019-Sept-WaterIsotopes.csv") 
-tibble(df)
+df<-read.csv("./2019-Sept-WaterIsotopes.csv")
+## BMB: we like spaces around '<-' for readability
+tibble(df) ## BMB: if you want to use tibbles anyway, you can use read_csv()
+## instead of read.csv()
+## why not just convert to a tibble? (this _views as_ as tibble but
+##  doesn't save it)
 str(df)
+## BMB: note there are lots of blanks in Contaminant: is that on purpose?
 
 siteinfo_table<-read.csv(text="
 Site,Site2,Treatment,Location
@@ -20,11 +25,25 @@ S5,U02,Spray,U
 S4,U01,Defoliated,U
 S3,L08,Spray,L
 S2,L12,Spray,L
-S1,L10,Spray,L", header=T, stringsAsFactors=T)
+S1,L10,Spray,L", header=TRUE, stringsAsFactors=TRUE)
+## BMB: always use TRUE/FALSE instead of T/F
 
 # 0. Massage Data #
 df<-separate(df, "Sample", c("Site","Rep"), sep="919KKW") #Split Sample ID - unfortunately the easiest way removes 919
-df$Sample<-c("919") # Sample=919 means that it was collected Aug 2019
+## BMB: it's too bad that there are no separators *and* that the terms differ in
+## length. If the ID were always 3 digits ("N06", "P20" and the data were always four
+## digites "0919", then separate() would let you separate by numerical position
+df$Sample <- c("919") # Sample=919 means that it was collected Aug 2019
+## BMB: this is a little dangerous, because it's hard-coded
+
+## BMB: the weird ?= things are *lookahead* and
+## lookbehind regular expressions: they split on 919 without removing it.
+## this is still bad because we have the date hardcoded, and we can't make
+## any general conclusions about where the split will be if we don't know
+## how many digits are in the Sample ID vs the date ...
+separate(df, "Sample", c("Site","tmp"), sep="(?=919)") %>%
+    separate("tmp", c("date","Rep"), sep="(?<=919)")
+    
 df$Site<-as.factor(df$Site)
 df$Sample<-as.factor(df$Sample)
 df.complete <- df %>% 
@@ -47,6 +66,10 @@ print(df.clean %>%
 
 ## Grade 1.9/3
 
+## BMB: you could save() or saveRDS() what you had done up to this point
+## and separate your scripts, using load() or readRDS() to get the clean
+## data back into R ...
+
 # 2. Data Exploration #
 # Compare Spray and Defoliated sites #
 
@@ -64,7 +87,9 @@ df.clean %>%
   ggplot(aes(d2H)) +
   geom_histogram(binwidth = 3, fill="lightblue", color="black") +
   ggtitle("Distribution of d2H Isotopes") +
-  scale_y_continuous(breaks = seq(0, 11, by=1)) 
+    scale_y_continuous(breaks = seq(0, 11, by=1))
+
+## BMB: how did you choose your binwidth?
 
 df.clean %>%  # wrap by Site now
   ggplot(aes(d2H)) +
@@ -74,3 +99,10 @@ df.clean %>%  # wrap by Site now
   facet_wrap(~Site)
 
 
+## BMB: these are reasonable pictures, but it's not clear to me what you're
+## looking for in terms of quality control of your data. I guess they're probably
+## clean already (or you think they are), so you're not worried.  The graphical
+## presentations _will_ help identify outliers etc.,
+## although multivariate views will be more powerful than univariate views
+
+## Grade: 2/3
--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 For this dataset, I would like to explore any patterns in the isotope signatures in the water. I would like to explore whether there are differences between my 2 treatments (defoliated vs spray sites) or between the different locations (upper, central, and lower). For this exploratory data analysis, I will use a combination of graphing (boxplots, bar graphs, etc) and statistics (ie. T-tests)
 
+**BMB:** as we will see, "whether there are any differences" is usually the wrong question. "Whether there are **clear** differences" (i.e. we can tell whether isotopic signatures change in one direction or the other) or "What are the differences?" or "Can we detect the difference we expect from theory and previous work" ...?
+
+**BMB**: In general, what do H and O isotopic signatures tell us about the biology/ecology of a particular place?
